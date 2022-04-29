4148c5dadf10b5c0693a6f05bf8b8a1d79649d39
abisoye <beesoye111@gmail.com>
2022-04-29 18:49:36 +0100

    use of jQuery

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("#thanks").submit(function(event) {
+    event.preventDefault ();
+
+    const adjective = $("#adjective").val();
+    const type = $("#type").val();
+    const room = $("#room").val();
+    
+    
+
+
+
+
+
+
+
+});
+
+
+});
